f168a388f037172c44bb4fa94634bd0a93c6da76
John Ramey <john.ramey@pnnl.gov>
2011-11-28 16:33:17 -0800

    Added the beginnings of a unit test to go along with Issue #1 on github.

--- inst/tests/1.R
+++ inst/tests/1.R
@@ -1,32 +1,8 @@
-library('bootclust')
+library('testthat')
+library('clusteval')
 
-context("Three bivariate Gaussian with equal, identity cov matrices")
+context("More cluster centers than distinct points.")
 
-normal <- function(N = 30, p = 2, delta = 1, probs = c(1,1,1)/3) {
- stopifnot(sum(probs) == 1) 
-}
-
-k <- 3
-delta <- 1
-p <- 2 
-n1 <- 20
-n2 <- 20
-n3 <- 20
-mu1 <- 1 * rep(1,p) * delta
-mu2 <- 2 * rep(1,p) * delta
-mu3 <- 3 * rep(1,p) * delta
-Sigma <- diag(p)
-
-x <- rbind(rmvnorm(n1, mu1, Sigma), rmvnorm(n2, mu2, Sigma), rmvnorm(n3, mu3, Sigma))
-
-# Distances for clustering
-euc_dist_x <- dist(x)
-man_dist_x <- dist(x, method = "manhattan")
-
-hc_ave_euc <- cutree(hclust(euc_dist_x, "ave"), k = 3)
-hc_ave_man <- cutree(hclust(man_dist_x, "ave"), k = 3)
-hc_single_euc <- cutree(hclust(euc_dist_x, "single"), k = 3)
-hc_single_man <- cutree(hclust(man_dist_x, "single"), k = 3)
-hc_compl_euc <- cutree(hclust(euc_dist_x, "compl"), k = 3)
-hc_compl_man <- cutree(hclust(man_dist_x, "compl"), k = 3)
-kmeans_out <- kmeans(x, centers = 3)
+test_that("The number of cluster centers does not exceed the number of distinct points", {
+  
+})
