c53e091a4e0615f4c314534706684c62111b3c59
John Ramey <johnramey@gmail.com>
2012-08-28 10:15:07 -0700

    Updated data-generating functions and overhauled their documentation. Closes #2

--- R/sim_normal.r
+++ R/sim_normal.r
@@ -1,54 +1,67 @@
-#' Generates multivariate normal populations with intraclass covariance matrices.
+#' Generates random variates from multivariate normal populations with intraclass
+#' covariance matrices.
 #'
-#' We generate \eqn{n_m} observations \eqn({m = 1, \ldots, M})
-#' from each of \eqn{M} multivariate normal
-#' distributions such that the Euclidean distance between
-#' the means of each of the populations is a fixed constant, \eqn{\Delta > 0}.
-#'
-#' By default, we consider \eqn{M = 5} populations.
+#' We generate \eqn{n_m} observations \eqn{(m = 1, \ldots, M)} from each of
+#' \eqn{M} multivariate normal distributions such that the Euclidean distance
+#' between each of the means and the origin is equal and scaled by
+#' \eqn{\Delta \ge 0}.
 #'
 #' Let \eqn{\Pi_m} denote the \eqn{m}th population with a \eqn{p}-dimensional
 #' multivariate normal distribution, \eqn{N_p(\mu_m, \Sigma_m)} with mean
-#' vector \eqn{\mu_m} and covariance matrix \eqn{\Sigma_m}. Also, let
-#' \eqn{z_1, \ldots, z_M \sim N_p(0_p, \epsilon I_p)} be independently
-#' distributed vectors of noise for a specified \eqn{\epsilon > 0}, where
-#' \eqn{0_p} is a \eqn{p \times 1} vector of zeros. Also, let \eqn{e_m} be
-#' the \eqn{m}th standard basis vector (i.e. the \eqn{m}th element is 1 and
-#' the remaining values are 0). Then, we define \eqn{\mu_m = \Delta e_m + z_m}.
-#' We introduce noise to the means to allow for more realistic simulated data set.
+#' vector \eqn{\mu_m} and covariance matrix \eqn{\Sigma_m}. Also, let \eqn{e_m}
+#' be the \eqn{m}th standard basis vector (i.e., the \eqn{m}th element is 1 and
+#' the remaining values are 0). Then, we define
+#' \deqn{\mu_m = \Delta \sum_{j=1}^{p/M} e_{(p/M)(m-1) + j}.} Note that \code{p}
+#' must be divisible by \code{M}. By default, the first 10 dimensions of
+#' \eqn{\mu_1} are set to \code{delta} with all remaining dimensions set to 0,
+#' the second 10 dimensions of \eqn{\mu_2} are set to \code{delta} with all
+#' remaining dimensions set to 0, and so on.
 #'
-#' By default, we let \eqn{\Delta = 0} and \eqn{\epsilon = 0.1}.
-#'
-#' Also, we considered intraclass covariance (correlation) matrices such that
+#' Also, we consider intraclass covariance (correlation) matrices such that
 #' \eqn{\Sigma_m = \sigma^2 (1 - \rho_m) J_p + \rho_m I_p}, where
-#' \eqn{-(p-1)^{-1} < \rho_m < 1}, \eqn{I_p} is the \eqn{p \times p} identity matrix,
-#' and \eqn{J_p} denotes the \eqn{p \times p} matrix of ones.
+#' \eqn{-(p-1)^{-1} < \rho_m < 1}, \eqn{I_p} is the \eqn{p \times p} identity
+#' matrix, and \eqn{J_p} denotes the \eqn{p \times p} matrix of ones.
 #'
-#' By default, we let \eqn{\sigma^2 = 1}.
+#' By default, we let \eqn{M = 5}, \eqn{\Delta = 0}, and \eqn{\sigma^2 = 1}.
+#' Furthermore, we generate 25 observations from each population by default.
 #'
-#' We generate \eqn{n_m} observations from population \eqn{\Pi_m}. By default, we
-#' generate 25 observations from each population.
+#' For \eqn{\Delta = 0} and \eqn{\rho_m = \rho}, \eqn{m = 1, \ldots, M}, the
+#' \eqn{M} populations are equal.
 #'
 #' @param n a vector (of length M) of the sample sizes for each population
 #' @param p the dimension of the multivariate normal populations
-#' @param rho a vector (of length M) of the intraclass constants for each population
-#' @param delta the fixed distance between each population
-#' @param epsilon the amount of noise to add to the population means.
+#' @param rho a vector (of length M) of the intraclass constants for each
+#' population
+#' @param delta the fixed distance between each population and the origin
 #' @param sigma2 the coefficient of each intraclass covariance matrix
-#' @param seed Seed for random number generation. (If NULL, does not set seed)
-#' @return data.frame. The 'Population' column denotes the population from which
-#' the observation in each row was generated. The remaining columns in each row
-#' contain the generated observation.
+#' @param seed seed for random number generation (If NULL, does not set seed)
+#' @return named list containing:
+#' \describe{
+#'   \item{x:}{A matrix whose rows are the observations generated and whose
+#'   columns are the \code{p} features (variables)}
+#'   \item{y:}{A vector denoting the population from which the observation in
+#'   each row was generated.}
+#' }
 #' @export
 #' @examples
-#' TODO
-sim_normal <- function(n = rep(25, 5), p = 50, rho = 0.1 * seq.int(1, 9, by = 2),
-delta = 0, epsilon = 1, sigma2 = 1, seed = NULL) {
+#' data_generated <- sim_normal(n = 10 * seq_len(5), seed = 42)
+#' dim(data_generated$x)
+#' table(data_generated$y)
+#'
+#' data_generated2 <- sim_normal(p = 10, delta = 2, rho = rep(0.1, 5))
+#' table(data_generated2$y)
+#' sample_means <- with(data_generated2,
+#'                      tapply(seq_along(y), y, function(i) {
+#'                             colMeans(x[i,])
+#'                      }))
+#' (sample_means <- do.call(rbind, sample_means))
+sim_normal <- function(n = rep(25, 5), p = 50, rho = rep(0.9, 5), delta = 0,
+                       sigma2 = 1, seed = NULL) {
+  # The number of populations
+  M <- length(n)
+
   if (delta < 0) {
     stop("The value for 'delta' must be a nonnegative constant.")
-  }
-  if (epsilon < 0) {
-    stop("The value for 'epsilon' must be a nonnegative constant.")
   }
   if (sigma2 <= 0) {
     stop("The value for 'sigma2' must be positive.")
@@ -56,42 +69,37 @@ delta = 0, epsilon = 1, sigma2 = 1, seed = NULL) {
   if (length(n) != length(rho)) {
     stop("The length of the vectors 'n' and 'rho' must be equal.")
   }
+  if (p %% M != 0) {
+    stop("We require that 'p' be divisible by 'M'")
+  }
   if(!is.null(seed)) {
     set.seed(seed)
   }
 
-  # The number of populations
-  M <- length(n)
-
-  # The multivariate normal noise
-  z <- rmvnorm(M, sigma = epsilon * diag(p))
-
   # A matrix whose rows are the population means.
   means <- lapply(seq.int(M), function(m) {
     mu_m <- matrix(0, nrow = M, ncol = p / M)
-    mu_m[m, ] <- 1
+    mu_m[m, ] <- delta
     mu_m
   })
-  means <- delta * do.call(cbind, means)
+  means <- do.call(cbind, means)
 
   # A list containing each of the covariance matrices.
-  # TODO: This may not be very practical for large p.
-  #       Use another approach.
   cov_list <- lapply(rho, intraclass_cov, p = p)
 
   # Generates the data in a list of length M.
   # Then, we rbind the data together.
   x <- lapply(seq_len(M), function(m) {
-    cbind(m, rmvnorm(n[m], means[m, ] + z[m, ], cov_list[[m]]))
+    rmvnorm(n[m], means[m, ], cov_list[[m]])
   })
-  x <- do.call(rbind.data.frame, x)
-  
-  colnames(x) <- c("Population", paste("x", seq.int(ncol(x) - 1), sep = ""))
-  x$Population <- as.factor(x$Population)  
-  x
+  x <- do.call(rbind, x)
+  y <- do.call(c, sapply(seq_len(M), function(m) {
+    rep.int(m, n[m])
+  }, simplify = FALSE))
+  list(x = x, y = y)
 }
 
-#' Construct an intraclass covariance (correlation) matrix.
+#' Construct an intraclass covariance matrix.
 #'
 #' We define a \eqn{p \times p} intraclass covariance (correlation)
 #' matrix to be \eqn{\Sigma_m = \sigma^2 (1 - \rho) J_p + \rho I_p},
@@ -102,7 +110,7 @@ delta = 0, epsilon = 1, sigma2 = 1, seed = NULL) {
 #' @param p the dimension of the matrix
 #' @param rho the intraclass covariance (correlation) constant
 #' @param sigma2 the coefficient of the intraclass covariance matrix
-#' @return a square matrix of size p
+#' @return an intraclass covariance matrix matrix of size p
 intraclass_cov <- function(p, rho, sigma2 = 1) {
   if (rho <= -(p-1)^(-1) || rho >= 1) {
     stop("The value for 'rho' must be exclusively between -1 / (p - 1) and 1.")
--- R/sim_unif.r
+++ R/sim_unif.r
@@ -1,35 +1,54 @@
-#' Generates four trivariate uniform populations.
+#' Generates random variates from five multivariate uniform populations.
 #'
-#' We generate 'n' observations from each of four trivariate
+#' We generate \code{n} observations from each of four trivariate
 #' distributions such that the Euclidean distance between
-#' each of the populations is a fixed constant, 'delta' > 0.
+#' each of the populations is a fixed constant, \code{delta} > 0.
 #'
-#' To define the populations, let \eqn{X_1 \sim U(a_1, b_1)}
-#' and \eqn{X_2 \sim U(a_2, b_2)} be independently distributed
-#' uniform random variables with \eqn{a_1 < b_1} and \eqn{a_2 < b_2}.
-#' Then, we denote the (independent) trivariate uniform distribution
-#' as \eqn{X = (X_1, X_2)' \sim U(a_1, b_1) \times U(a_2, b_2)}.
-#' Let \eqn{Pi_m} denote the \eqn{m}th population \eqn{(m = 1, \ldots, 4)}.
-#' Then, we have the four populations:
-#' \deqn{
-#'  \Pi_1 = U(-1/2, 1/2) \times U(\Delta - 1/2, \Delta + 1/2),
-#'  \Pi_2 = U(\Delta - 1/2, \Delta + 1/2) \times U(-1/2, 1/2),
-#'  \Pi_3 = U(-1/2, 1/2) \times U(-\Delta - 1/2, -\Delta + 1/2),
-#'  \Pi_4 = U(-\Delta - 1/2, -\Delta + 1/2) \times U(-1/2, 1/2).
-#' }
-#' TODO: Update the populations to be trivariate.
+#' To define the populations, let \eqn{x = (X_1, \ldots, X_p)'} be a
+#' multivariate uniformly distributed random vector such that
+#' \eqn{X_j \sim U(a_j, b_j)} is an independently distributed uniform random
+#' variable with \eqn{a_j < b_j} for \eqn{j = 1, \ldots, p}. Let \eqn{Pi_m}
+#' denote the \eqn{m}th population \eqn{(m = 1, \ldots, 5)}. Then, we have the
+#' five populations:
+#' \deqn{\Pi_1 = U(-1/2, 1/2) \times U(\Delta - 1/2, \Delta + 1/2) \times U(-1/2, 1/2) \times U(-1/2, 1/2),}
+#' \deqn{\Pi_2 = U(\Delta - 1/2, \Delta + 1/2) \times U(-1/2, 1/2) \times U(-1/2, 1/2) \times U(-1/2, 1/2),}
+#' \deqn{\Pi_3 = U(-1/2, 1/2) \times U(-\Delta - 1/2, -\Delta + 1/2) \times U(-1/2, 1/2) \times U(-1/2, 1/2),}
+#' \deqn{\Pi_4 = U(-1/2, 1/2) \times U(-1/2, 1/2) \times U(-\Delta - 1/2, -\Delta + 1/2) \times U(-1/2, 1/2),}
+#' \deqn{\Pi_5 = U(-1/2, 1/2) \times U(-1/2, 1/2) \times U(-1/2, 1/2) \times U(\Delta - 1/2, \Delta + 1/2).}
 #'
-#' @param n sample size of each population
-#' @param delta the fixed distance between each population
+#' We generate \eqn{n_m} observations from population \eqn{\Pi_m}.
+#'
+#' For \eqn{\Delta = 0} and \eqn{\rho_m = \rho}, \eqn{m = 1, \ldots, M}, the
+#' \eqn{M} populations are equal.
+#'
+#' Notice that the support of each population is a unit hypercube with 4
+#' features. Moreover, for \eqn{\Delta \ge 1}, the populations are mutually
+#' exclusive and entirely separated.
+#'
+#' @param n a vector (of length M = 5) of the sample sizes for each population
+#' @param delta the fixed distance between each population and the origin
 #' @param seed Seed for random number generation. (If NULL, does not set seed)
-#' @return data.frame. The 'Population' column denotes the population from which
-#' the observation in each row was generated. The remaining columns in each row
-#' contain the generated observation.
+#' @return named list containing:
+#' \describe{
+#'   \item{x:}{A matrix whose rows are the observations generated and whose
+#'   columns are the \code{p} features (variables)}
+#'   \item{y:}{A vector denoting the population from which the observation in
+#'   each row was generated.}
+#' }
 #' @export
 #' @examples
-#' x <- sim_unif(50, delta = 1.5)
-#' plyr:::ddply(x, .(Population), summarize, xbar1 = mean(x1), xbar2 = mean(x2), xbar3 = mean(x3))
-sim_unif <- function(n = 25, delta = 0, seed = NULL) {
+#' data_generated <- sim_unif(seed = 42)
+#' dim(data_generated$x)
+#' table(data_generated$y)
+#'
+#' data_generated2 <- sim_unif(n = 10 * seq_len(5), delta = 1.5)
+#' table(data_generated2$y)
+#' sample_means <- with(data_generated2,
+#'                      tapply(seq_along(y), y, function(i) {
+#'                             colMeans(x[i,])
+#'                      }))
+#' (sample_means <- do.call(rbind, sample_means))
+sim_unif <- function(n = rep(25, 5), delta = 0, seed = NULL) {
   if (delta < 0) {
     stop("The value for 'delta' must be a nonnegative constant.")
   }
@@ -43,16 +62,26 @@ sim_unif <- function(n = 25, delta = 0, seed = NULL) {
   pop4 <- c(-1/2, 1/2, -1/2, 1/2, delta - 1/2, delta + 1/2, -1/2, 1/2)
   pop5 <- c(-1/2, 1/2, -1/2, 1/2, -1/2, 1/2, delta - 1/2, delta + 1/2)  
   
-  unif_pops <- rbind.data.frame(pop1, pop2, pop3, pop4, pop5)
-  colnames(unif_pops) <- c("a1", "b1", "a2", "b2", "a3", "b3", "a4", "b4")
-  unif_pops$n <- n
-  
-  multivariate_unif <- function(n, a1, b1, a2, b2, a3, b3, a4, b4) {
-    cbind(runif(n, a1, b1), runif(n, a2, b2), runif(n, a3, b3), runif(n, a4, b4))
+  unif_pops <- rbind(pop1, pop2, pop3, pop4, pop5)
+  M <- nrow(unif_pops)
+
+  # Helper function that generates 'n' observations from a multivariate uniform
+  # distribution with the configuration given in the description above. The
+  # 'unif_params' argument is a vector of length 8 that contains each of the
+  # a_j and b_j described above.
+  multivariate_unif <- function(n, unif_params) {
+    cbind(runif(n, unif_params[1], unif_params[2]),
+          runif(n, unif_params[3], unif_params[4]),
+          runif(n, unif_params[5], unif_params[6]),
+          runif(n, unif_params[7], unif_params[8]))
   }
   
-  x <- mdply(unif_pops, as.data.frame(multivariate_unif), .expand = F)
-  colnames(x) <- c("Population", "x1", "x2", "x3", "x4")
-  x$Population <- as.factor(x$Population)  
-  x
+  x <- lapply(seq_len(M), function(m) {
+    multivariate_unif(n[m], unif_pops[m, ])
+  })
+  x <- do.call(rbind, x)
+  y <- do.call(c, sapply(seq_len(M), function(m) {
+    rep.int(m, n[m])
+  }, simplify = FALSE))
+  list(x = x, y = y)
 }
